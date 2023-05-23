226cb5073d7afa69dfc7b5087aa8ef32f37d1882
harunaabaldeh <harunaabaldeh@gmail.com>
2023-05-23 17:12:58 +0000

    complete user login and register client page

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -9,7 +9,7 @@ import { User } from '../_models/user';
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
   private currentUserSource = new BehaviorSubject<User | null>(null);
-  private currentUser$ = this.currentUserSource.asObservable();
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) {}
 
@@ -17,6 +17,17 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user) => {
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,33 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn = false;
+ 
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) {}
 
   ngOnInit(): void {
   }
 
-  login(){
+  login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.loggedIn = true;
-        
+      next: (response) => {
+        console.log('response ', response);
       },
-      error: error => console.log(error)
-    })
+      error: (error) => console.log(error),
+    });
   }
 
-  logout(){
-    this.loggedIn = false;
+  logout() {
+    this.accountService.logout();
   }
-
 }
